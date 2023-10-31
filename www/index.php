7b70b47186f8ecdda564b0fd885c75a2b3424718
Sina Eetezadi <code@eetezadi.com>
2023-10-31 16:40:05 +0000

    small fixes

--- www/index.php
+++ www/index.php
@@ -1,7 +1,7 @@
 <body>
 <h1>Welcome to LAMP on Gitpod!</h1>
 <P>Files will be served from "www"</P>
-<P>Open the <a href="<?php echo $GITPOD_WORKSPACE_URL ?>/phpmyadmin">PhpMyAdmin<a> in the <b>browser</b> to work on the DBs. You may want to change the default user gitpod:gitpod.</P>
+<P>Open the <a href="<?php echo $GITPOD_WORKSPACE_URL ?>/phpmyadmin">PhpMyAdmin<a> in the <b>browser</b> to work on the DBs (takes a while to install, check the terminal). You may want to change the default user gitpod:gitpod.</P>
 <P>The mySQL database stores in /workspace/mysql. So data is persistent as long as workspace exists.</P>
 <P>Check phpinfo.php for environment and debug_example.php to test the debugger.</P>
 </body>
